f331c72ab6487fdb05e3898b3435b5b00446634b
AbhijitG <abhijitgupta61@gmail.com>
2020-07-03 16:43:09 +0530

    Angular file added

---
+++ app.js
@@ -0,0 +1,14 @@
+/*
+ AngularJS v1.8.0
+ (c) 2010-2020 Google, Inc. http://angularjs.org
+ License: MIT
+*/
+(function () {
+'use strict';
+angular.module('MyApp',[])
+.controller('MyController',function($scope)
+{
+  $scope.name="Jio";
+  $scope.Address1="Kolkata";
+})
+})();
